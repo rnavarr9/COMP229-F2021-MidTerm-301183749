63fe387516f05faa1511fa0e16dd20e301528664
rnavarr9 <rnavarr9@my.centennialcollege.ca>
2021-10-26 16:04:28 -0400

    Updates get main endpoint to retrieve books from mongo db

--- server/routes/books.js
+++ server/routes/books.js
@@ -11,20 +11,16 @@ let book = require('../models/books');
 /* GET books List page. READ */
 router.get('/', (req, res, next) => {
   // find all books in the books collection
-  // book.find( (err, books) => {
-  //   if (err) {
-  //     return console.error(err);
-  //   }
-  //   else {
-  //     res.render('books/index', {
-  //       title: 'Books',
-  //       books: books
-  //     });
-  //   }
-  // });
-  res.render('books/index', {
-    title: 'Books',
-    books: books.books
+  book.find( (err, books) => {
+    if (err) {
+      return console.error(err);
+    }
+    else {
+      res.render('books/index', {
+        title: 'Books',
+        books: books
+      });
+    }
   });
 });
 
